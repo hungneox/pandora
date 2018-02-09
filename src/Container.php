0095566ddfc207ab9507b8f0beb4360523e34822
Hung Neo <hungnq1989@gmail.com>
2018-02-09 23:55:09 +0200

    Put Pandora under Neox namespace

--- src/Container.php
+++ src/Container.php
@@ -1,27 +1,45 @@
 <?php
 
-namespace Pandora;
+namespace Neox\Pandora;
 
+/**
+ * Class Container
+ * @package Neox\Pandora
+ */
 class Container {
 
     public $registry = [];
     
     public static $instance = null;
 
+    /**
+     * @param $key
+     * @param $callback
+     */
     protected function register($key, $callback) 
     {
         $this->registry[$key] = $callback();
     }
 
+    /**
+     * @param $key
+     * @return mixed
+     * @throws \Exception
+     */
     protected function resolve($key) 
     {
         if(empty($this->registry[$key])) {
-            throw new Exception("Cannot resolve your class", 1);
+            throw new \Exception("Cannot resolve your class", 1);
         }
 
         return $this->registry[$key];
     }
 
+    /**
+     * @param $method
+     * @param $args
+     * @return mixed
+     */
     public static function __callStatic($method, $args)
     {
         if (static::$instance === null) {
